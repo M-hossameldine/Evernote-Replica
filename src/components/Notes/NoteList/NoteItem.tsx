bda401e1d56b2827c4856feaf253a8c3e03321df
Mohamed Hossam <m.hossameldine@gmail.com>
2022-04-25 01:32:45 +0200

    feat(style): editor active list NoteItem style

--- src/components/Notes/NoteList/NoteItem.tsx
+++ src/components/Notes/NoteList/NoteItem.tsx
@@ -1,40 +1,49 @@
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import classes from './NoteItem.module.css';
 
 import { NOTE_INTERFACE } from '../../../interfaces/note-interface';
 import { TRASH_ITEM_INTERFACE } from '../../../interfaces/trash-interface';
-// import { NOTESPAGE } from '../../../constants/routes';
 import { useLocationIndicator } from '../../../hooks/use-locationIndicator';
 
 const NoteItem: React.FC<{
   note: NOTE_INTERFACE | TRASH_ITEM_INTERFACE;
-  onClick?: (noteId: string) => void;
+  index: number;
+  onClick?: (noteIndex: number) => void;
 }> = (props) => {
+  const params = useParams();
   const location = useLocationIndicator();
-  const { note, onClick } = props;
+  const { note, index, onClick } = props;
 
   let { text, title, createdTimestamp } = 'note' in note ? note.note : note;
 
-  const createNoteTimestamp = new Date(createdTimestamp);
-  // const { id: selectedNoteId } = 'note' in note ? note : note;
-  const { id: selectedNoteId } = note;
+  // const { id: localNoteId } = 'note' in note ? note : note;
+  const { id: localNoteId } = note;
 
+  const noteFocusHandler = () => {
+    if (props.onClick) {
+      props.onClick(index);
+    }
+  };
+
+  const createNoteTimestamp = new Date(createdTimestamp);
   const noteTimestampValue = `${createNoteTimestamp.toLocaleString('default', {
     month: 'short',
   })} ${createNoteTimestamp.getUTCDate()}`;
 
-  const noteFocusHandler = () => {
-    if (props.onClick) {
-      props.onClick(selectedNoteId);
-    }
-  };
-
-  console.log('route', `${location.locationKey}/${selectedNoteId}`);
+  let noteItemClasses = ` ${classes.note} `;
+  if (localNoteId === params.noteId) {
+    noteItemClasses += ` ${classes.active} `;
+  }
+  //  + ()
+  //   ?
+  //   : '';
 
   return (
     <Link
-      className={classes.note}
-      to={`/${location.locationKey}/${selectedNoteId}`}
+      // className={`${classes.note} ${classes.active}`}
+      className={noteItemClasses}
+      to={`/${location.locationKey}/${localNoteId}`}
+      onClick={noteFocusHandler}
     >
       <h3 className={classes['note__title']}>
         {title.length > 0 ? title : 'Untitled'}
--- src/components/Notes/NoteList/NoteList.tsx
+++ src/components/Notes/NoteList/NoteList.tsx
@@ -1,4 +1,5 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from '../../../hooks/redux-hooks';
 import { useLocationIndicator } from '../../../hooks/use-locationIndicator';
@@ -13,11 +14,20 @@ import { NOTESPAGE } from '../../../constants/routes';
 const NoteList: React.FC<{
   notes: (NOTE_INTERFACE | TRASH_ITEM_INTERFACE)[];
 }> = (props) => {
-  const dispatch = useAppDispatch();
-  const navigate = useNavigate();
-  const { locationKey } = useLocationIndicator();
-
+  // const dispatch = useAppDispatch();
+  // const navigate = useNavigate();
+  // const { locationKey } = useLocationIndicator();
+  // store index value of active item to be used after deleting any note
+  const [activeNoteIndex, setActiveNoteIndex] = useState(0);
+  const params = useParams();
+  // const [  ] = useState()
   const { notes } = props; // received
+
+  useEffect(() => {}, []);
+
+  const selectedNoteHandler = (noteIndex: number) => {
+    setActiveNoteIndex(noteIndex);
+  };
 
   // const activateNoteHandler = (noteId: string) => {
   //   const existedNote = notes.find((note) => note.id === noteId)!;
@@ -30,8 +40,13 @@ const NoteList: React.FC<{
   return (
     <div className='scrollbar-box overflow-scroll overflow-x-hidden '>
       <ul className='relative grow flex flex-col gap-3 p-3 pb-6 bg-neutral-100 cursor-pointer'>
-        {notes.map((note) => (
-          <NoteItem key={note.id} note={note} />
+        {notes.map((note, index) => (
+          <NoteItem
+            key={note.id}
+            note={note}
+            index={index}
+            onClick={selectedNoteHandler}
+          />
         ))}
       </ul>
     </div>
