12d854452babb79f135190dbdcc7b71955191ad5
Mohamed Hossam <m.hossameldine@gmail.com>
2022-04-28 15:53:42 +0200

    fix: refactor addNewNote tab at sideNavbar

--- src/components/Notes/AddNoteWrapper/AddNoteWrapper.tsx
+++ src/components/Notes/AddNoteWrapper/AddNoteWrapper.tsx
@@ -3,9 +3,13 @@ import { selectNotes } from '../../../store/notes-slice/notes-slice';
 import { sendNewNoteData } from '../../../store/notes-slice/notes-actions';
 import { NOTESPAGE } from '../../../constants/routes';
 import { useUpdatedState } from '../../../hooks/use-updatedState';
-const AddNewNote: React.FC<{ payload?: {}; className?: string }> = (props) => {
-  const { payload, className } = props;
+const AddNoteWrapper: React.FC<{
+  actionPayload?: {};
+  className?: string;
+}> = (props) => {
+  const { actionPayload, className } = props;
   const notes = useAppSelector(selectNotes);
+
   const notesUpdatedState = useUpdatedState({
     asyncAction: sendNewNoteData,
     route: NOTESPAGE,
@@ -15,13 +19,14 @@ const AddNewNote: React.FC<{ payload?: {}; className?: string }> = (props) => {
   });
 
   const addNoteHandler = () => {
-    notesUpdatedState.dispatchActionHandler({ ...payload });
+    notesUpdatedState.dispatchActionHandler({ ...actionPayload });
   };
+
   return (
-    <div className={className ? className : ''} onClick={addNoteHandler}>
+    <button className={className ? className : ''} onClick={addNoteHandler}>
       {props.children}
-    </div>
+    </button>
   );
 };
 
-export default AddNewNote;
+export default AddNoteWrapper;
--- src/components/SideNavBar/AddNewNoteTab.tsx
+++ src/components/SideNavBar/AddNewNoteTab.tsx
@@ -1,38 +1,20 @@
-import { useAppSelector } from '../../hooks/redux-hooks';
-import { selectNotes } from '../../store/notes-slice/notes-slice';
-import { sendNewNoteData } from '../../store/notes-slice/notes-actions';
-import { NOTESPAGE } from '../../constants/routes';
-import { useUpdatedState } from '../../hooks/use-updatedState';
+import AddNoteWrapper from '../Notes/AddNoteWrapper/AddNoteWrapper';
 import Icons from '../../constants/Icons';
 
 const { BsPlus, IoIosArrowDown } = Icons;
 
 const AddNewNoteTab: React.FC = () => {
-  const notes = useAppSelector(selectNotes);
-  const notesUpdatedState = useUpdatedState({
-    asyncAction: sendNewNoteData,
-    route: NOTESPAGE,
-    usedIndex: 0,
-    watchedState: notes,
-    operation: 'add',
-  });
-
-  const addNoteHandler = () => {
-    notesUpdatedState.dispatchActionHandler();
-  };
-
   return (
-    <button
+    <AddNoteWrapper
       aria-label='New Note'
       className='flex items-center justify-center lg:justify-evenly text-white py-[6px] lg:px-2 rounded-3xl w-8 lg:w-auto aspect-square lg:aspect-auto bg-green-550 hover:opacity-90 cursor-pointer'
-      onClick={addNoteHandler}
     >
       <BsPlus className='text-2xl shrink-0 mx-auto lg:mx-[unset]' />
       <span className='hidden lg:block'>New</span>
       <span className='hidden lg:block lg:ml-auto'>
         <IoIosArrowDown size='17' />
       </span>
-    </button>
+    </AddNoteWrapper>
   );
 };
 
