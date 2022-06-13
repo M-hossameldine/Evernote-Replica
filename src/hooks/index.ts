24496b2d1122b8beef98f15759e174f7aca2e6ec
Mohamed Hossam <m.hossameldine@gmail.com>
2022-06-13 16:39:35 +0200

    add event listener hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -3,3 +3,4 @@ export * from './use-outsideListener';
 export * from './use-updatedState';
 export * from './use-tokenData';
 export * from './redux-hooks';
+export * from './useEventListener';
---
+++ src/hooks/useEventListener.ts
@@ -0,0 +1,16 @@
+import { useEffect } from 'react';
+// for unit testing should only accepts dom events not any normal string
+
+export const useEventListener = (
+  event: string,
+  eventCallbackFn: () => void
+) => {
+  useEffect(() => {
+    window.addEventListener(event, eventCallbackFn);
+
+    // cleanup fn for boxshadow event listener
+    return () => {
+      window.removeEventListener(event, eventCallbackFn);
+    };
+  }, []);
+};
