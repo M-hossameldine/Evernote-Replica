5f426aa33a7a10661ba43dfb6480c77fe1d190ca
Mohamed Hossam <m.hossameldine@gmail.com>
2022-06-13 16:41:24 +0200

    fix(style): add box shadow to public main nav

--- src/components/navs/MainPublicNav/index.tsx
+++ src/components/navs/MainPublicNav/index.tsx
@@ -1,19 +1,34 @@
 import { useState } from 'react';
+import { useEventListener } from '../../../hooks';
 
 import { DesktopMenu, MobileMenu } from '../..';
 
 const MainPublicNav: React.FC = (props) => {
-  const [showMobileMenu, setShowMenu] = useState(false);
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
+  const [navBoxShadow, setNavBoxShadow] = useState(false);
+
+  // set Desktop Nav box shadow
+  const windowScrollHandler = () => {
+    window.scrollY >= 0 && window.scrollY <= 100
+      ? setNavBoxShadow(false)
+      : setNavBoxShadow(true);
+  };
+
+  useEventListener('scroll', windowScrollHandler);
 
   const toggleMobileMenu = () => {
-    setShowMenu((prevState) => {
+    setShowMobileMenu((prevState) => {
       console.log(prevState);
       return !prevState;
     });
   };
 
+  const boxShadowStyle = navBoxShadow ? 'shadow-md' : '';
+
   return (
-    <div className='fixed top-0 left-0 w-full text-gray-600 body-font'>
+    <div
+      className={`fixed top-0 left-0 w-full text-gray-600 bg-white ${boxShadowStyle} transition-all duration-100 body-font`}
+    >
       <DesktopMenu onShowMenu={toggleMobileMenu} />
       <MobileMenu onClose={toggleMobileMenu} showMenu={showMobileMenu} />
     </div>
