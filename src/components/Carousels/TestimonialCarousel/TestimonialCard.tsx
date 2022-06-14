d25de430f5e469ae6bae232e9d9056876201cfc3
Mohamed Hossam <m.hossameldine@gmail.com>
2022-06-14 21:24:03 +0200

    feat: add Testimonial Carousel

---
+++ src/components/Carousels/TestimonialCarousel/TestimonialCard.tsx
@@ -0,0 +1,29 @@
+import { FaQuoteLeft } from '../../../assets';
+
+// logo and altText are passed only if this card is part of the default carousel
+interface TestimonialCardProps {
+  review: string;
+  reviewer: string;
+  logo: string;
+  altText: string;
+}
+
+const TestimonialCard: React.FC<TestimonialCardProps> = (props) => {
+  const { review, reviewer, logo, altText } = props;
+  return (
+    <div className='text-lg sm:text-2xl sm:leading-[150%] text-center'>
+      <FaQuoteLeft className='text-lg sm:text-2xl shrink-0 text-green-600 mx-auto mb-[1.4em]' />
+      <p className='mb-[1.8em]'>"{review}"</p>
+      <p>— {reviewer}</p>
+
+      {/* logo img will be rendered in moble screen only */}
+      <img
+        className='block sm:hidden max-h-[1.8rem] mx-auto mt-12'
+        src={logo}
+        alt={altText}
+      />
+    </div>
+  );
+};
+
+export default TestimonialCard;
---
+++ src/components/Carousels/TestimonialCarousel/index.tsx
@@ -0,0 +1,59 @@
+import './index.css';
+
+import { TestimonialCard, LogoCarousel, DefaultCarousel } from '../../';
+import { Testimonial_CAROUSEL_DATA } from '../../../utils/data';
+import { Testimonial_CAROUSEL_DATA_INTERFACE } from '../../../interfaces';
+
+const DefaultCarouselSettings = {
+  dots: true,
+  infinite: true,
+  autoplay: true,
+  pauseOnHover: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: true,
+  customPaging: () => <button className='before:mt-4'></button>,
+};
+
+interface ComponentInterface {
+  className?: string;
+  data: Testimonial_CAROUSEL_DATA_INTERFACE[];
+  trackSettings: {};
+  viewSettings?: {};
+}
+const TestimonialCarousel: React.FC<ComponentInterface> = (props) => {
+  const { className, data, trackSettings, viewSettings } = props;
+
+  const carouselSlides = Testimonial_CAROUSEL_DATA.map((slide) => (
+    <TestimonialCard
+      key={slide.id}
+      review={slide.review}
+      reviewer={slide.reviewer}
+      logo={slide.logo}
+      altText={slide.altText}
+    />
+  ));
+
+  return (
+    <>
+      <div className={className ? className : ''}>
+        <div className='hidden sm:block'>
+          <LogoCarousel
+            data={data}
+            trackSettings={trackSettings}
+            viewSettings={viewSettings}
+          />
+        </div>
+        <div className='sm:hidden'>
+          <DefaultCarousel
+            slides={carouselSlides}
+            settings={DefaultCarouselSettings}
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default TestimonialCarousel;
