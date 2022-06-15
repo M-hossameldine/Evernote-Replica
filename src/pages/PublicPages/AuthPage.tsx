14281c976fb967dac739d9b12c50a96937205f8a
Mohamed Hossam <m.hossameldine@gmail.com>
2022-06-15 19:16:09 +0200

    move public pages to the public pages folder

---
+++ src/pages/PublicPages/AuthPage.tsx
@@ -0,0 +1,15 @@
+import { AuthForm } from '../../components';
+import { AuthBgImg } from '../../assets';
+
+const AuthPage: React.FC = (props) => {
+  return (
+    <div
+      className={`flex items-center h-screen `}
+      style={{ backgroundImage: `url(${AuthBgImg})` }}
+    >
+      <AuthForm />
+    </div>
+  );
+};
+
+export default AuthPage;
