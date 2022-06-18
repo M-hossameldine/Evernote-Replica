7606c74f418182734db381ad8309ec22183e9aac
Mohamed Hossam <m.hossameldine@gmail.com>
2022-06-18 14:26:28 +0200

    fix: download button style

--- src/pages/PublicPages/DownloadPage.tsx
+++ src/pages/PublicPages/DownloadPage.tsx
@@ -12,6 +12,7 @@ const DownloadPage: React.FC = (props) => {
           route:
             'https://cdn1.evernote.com/boron/win/builds/Evernote-10.36.4-win-ddl-ga-3377-6d2f121c6a-setup.exe',
           isExteranl: true,
+          className: 'text-lg font-bold tracking-widest px-[4.5em] ',
         }}
       />
     </div>
