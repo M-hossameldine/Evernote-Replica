5021f5e8ac8aedce639680051acce7caea93759f
Mohamed Hossam <m.hossameldine@gmail.com>
2023-05-20 14:17:53 +0200

    chore: add DefaultSpinner reusable component

---
+++ src/components/UI/Spinners/index.ts
@@ -0,0 +1,1 @@
+export { DefaultSpinner } from "./DefaultSpinner";
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,59 +1,31 @@
-// UI
-import ExecludeEventWrapper from './UI/ExecludeEventWrapper/ExecludeEventWrapper';
-import ExternalLink from './UI/Links/ExternalLink';
-import TextLink from './UI/Links/TextLink';
-import ContainedLink from './UI/Links/ContainedLink';
-import GhostLink from './UI/Links/GhostLink';
-import ReactIconLink from './UI/Links/ReactIconLink';
-import Notification from './UI/Notification/Notification';
-import DropdownMenu from './UI/DropdownMenu/';
-import Card from './UI/Card/Card';
-import RowCard from './UI/Card/RowCard/RowCard';
-import PricePlanCard from './UI/Card/PricePlanCard/PricePlanCard';
-import AutoGrowingTextArea from './UI/AutoGrowingTextArea/AutoGrowingTextArea';
-import LogoCarousel from './UI/carousels/LogoCarousel/LogoCarousel';
-import DefaultCarousel from './UI/carousels/DefaultCarousel/DefaultCarousel';
-import CenteredModal from './UI/modals/CenteredModal';
-
 // Public flow
-import AuthForm from './Auth/AuthForm';
-import MainPublicNav from './navs/MainPublicNav';
-import MobileMenu from './navs/MainPublicNav/MobileMenu';
-import DesktopMenu from './navs/MainPublicNav/DesktopMenu';
-import FeatureCarousel from './Carousels/FeatureCarousel/FeatureCarousel';
+import AuthForm from "./Auth/AuthForm";
+import MainPublicNav from "./navs/MainPublicNav";
+import MobileMenu from "./navs/MainPublicNav/MobileMenu";
+import DesktopMenu from "./navs/MainPublicNav/DesktopMenu";
+import FeatureCarousel from "./Carousels/FeatureCarousel/FeatureCarousel";
 
-import TestimonialCard from './UI/Card/TestimonialCard/TestimonialCard';
+import TestimonialCard from "./UI/Card/TestimonialCard/TestimonialCard";
 
 // User flow
-import Layout from './UI/Layout/Layout';
-import MainUserSideNav from './navs/MainUserSideNav';
-import SideNavTabs from './navs/MainUserSideNav/SideNavTabs';
-import HomePrivateHeader from './Home/HomePrivateHeader';
-import WidgetsContainer from './Home/WidgetsContainer';
-import NoteItem from './Notes/NoteItem/NoteItem';
-import AddNoteWrapper from './Notes/AddNoteWrapper/AddNoteWrapper';
-import AddNewNoteTab from './navs/MainUserSideNav/AddNewNoteTab';
-import NoteEditorHeader from './Notes/NoteEditor/NoteEditorHeader/NoteEditorHeader';
+import Layout from "./UI/Layout/Layout";
+import MainUserSideNav from "./navs/MainUserSideNav";
+import SideNavTabs from "./navs/MainUserSideNav/SideNavTabs";
+import HomePrivateHeader from "./Home/HomePrivateHeader";
+import WidgetsContainer from "./Home/WidgetsContainer";
+import NoteItem from "./Notes/NoteItem/NoteItem";
+import AddNoteWrapper from "./Notes/AddNoteWrapper/AddNoteWrapper";
+import AddNewNoteTab from "./navs/MainUserSideNav/AddNewNoteTab";
+import NoteEditorHeader from "./Notes/NoteEditor/NoteEditorHeader/NoteEditorHeader";
 
 // Other components
-import SimpleFeatureBlock from './other/FeatureBlock/SimpleFeatureBlock';
+import SimpleFeatureBlock from "./other/FeatureBlock/SimpleFeatureBlock";
 
 // Sections
-import HeroSection from '../sections/HeroSection/HeroSection';
+import HeroSection from "../sections/HeroSection/HeroSection";
 
+export * from "./UI";
 export {
-  ExecludeEventWrapper,
-  ExternalLink,
-  TextLink,
-  ContainedLink,
-  GhostLink,
-  ReactIconLink,
-  Notification,
-  DropdownMenu,
-  Card,
-  RowCard,
-  PricePlanCard,
-  AutoGrowingTextArea,
   AuthForm,
   MainPublicNav,
   DesktopMenu,
@@ -71,7 +43,4 @@ export {
   SimpleFeatureBlock,
   FeatureCarousel,
   TestimonialCard,
-  LogoCarousel,
-  DefaultCarousel,
-  CenteredModal,
 };
