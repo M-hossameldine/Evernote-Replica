e9634e98151b7ba0f40943af4885248db9dbde8a
M-hossameldine <m.hossameldine@gmail.com>
2023-11-26 23:31:23 +0200

    chore/add vitest configs

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 import svgrPlugin from "vite-plugin-svgr";
+import type { InlineConfig } from "vitest";
+import type { UserConfig } from "vite";
+
+interface VitestConfigExport extends UserConfig {
+  test: InlineConfig;
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,8 +15,16 @@ export default defineConfig({
     outDir: "build",
   },
   server: {
-    open: true, // Automatically open the app on server start
+    open: true, //  open app on server start
     port: 4500,
   },
   plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
-});
+  optimizeDeps: {
+    include: ["matchmedia-polyfill"], // Ensure this package is included in optimization
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFile: ["src/setupTests.ts"],
+  },
+} as VitestConfigExport);
