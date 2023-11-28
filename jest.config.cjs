b2c683935dc927e92ff16fb0b627a2705edb4ea6
M-hossameldine <m.hossameldine@gmail.com>
2023-11-28 12:57:11 +0200

    chore: add jest configuration

---
+++ jest.config.cjs
@@ -0,0 +1,12 @@
+// todo: investigate the need for this file
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["./test/setup.jest.ts"],
+  moduleNameMapper: {
+    "^\\$lib(.*)$": "<rootDir>/src/lib$1",
+    "^\\$components(.*)$": "<rootDir>/src/components$1",
+    "^test(.*)$": "<rootDir>/test$1",
+  },
+};
