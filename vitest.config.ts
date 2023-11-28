59420f582522b6d6f20c089b53bde397d68c5338
M-hossameldine <m.hossameldine@gmail.com>
2023-11-28 12:56:11 +0200

    chore: update vitest configurations

---
+++ vitest.config.ts
@@ -0,0 +1,32 @@
+import path from "node:path";
+import { defineConfig } from "vitest/config";
+import configuration from "./vite.config";
+
+export default defineConfig({
+  ...configuration,
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: path.resolve(__dirname, "test/setupTests.ts"),
+    environmentMatchGlobs: [
+      ["**/*.test.tsx", "jsdom"],
+      ["**/*.component.test.ts", "jsdom"],
+    ],
+    coverage: {
+      statements: 54.92,
+      thresholdAutoUpdate: true,
+      include: ["src/**/*"],
+      exclude: [
+        "test/**",
+        "vite.*.ts",
+        "**/*.d.ts",
+        "**/*.test.*",
+        "**/*.config.*",
+        "**/snapshot-tests/**",
+        "**/*.solution.tsx",
+        "**/coverage/**",
+      ],
+      all: true,
+    },
+  },
+});
