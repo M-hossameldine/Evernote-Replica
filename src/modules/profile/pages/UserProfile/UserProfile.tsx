f9e67ab5fc0470a00151c6b9352ba1c312654d62
M-hossameldine <m.hossameldine@gmail.com>
2024-12-09 22:33:48 +0200

    chore: move pages to modules

---
+++ src/modules/profile/pages/UserProfile/UserProfile.tsx
@@ -0,0 +1,12 @@
+import { HomePrivateHeader, WidgetsContainer } from "components";
+
+export const UserProfile = () => {
+  return (
+    <>
+      <HomePrivateHeader />
+      <WidgetsContainer />
+    </>
+  );
+};
+
+export default UserProfile;
