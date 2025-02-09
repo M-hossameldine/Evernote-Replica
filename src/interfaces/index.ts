a7c04825cd9f1366ce741021d73e1bae44c67fea
M-hossameldine <m.hossameldine@gmail.com>
2025-02-09 23:09:23 +0200

    fix: remove unused interfaces

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,5 +1,3 @@
-export * from "./auth-interfaces";
-export * from "./element-attributes-interface";
 export * from "./fallbackData-interface";
 export * from "./header-interface";
 export * from "./note-interface";
